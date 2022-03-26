d847d3b6446893b53ca79bbfd7e1cd4ee9266769
theruff <yagr13@mail.ru>
2022-03-26 21:45:54 +0100

    Add project files.

---
+++ SEM/FileOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CompressionMethods
+{
+        class FileReader
+    {
+        public static void readFile (string filePath)
+        {
+
+        }
+    }
+    class FileWriter
+    {
+
+    }
+    class TreeSerializer
+    {
+
+    }
+    class TreeDeserializer
+    {
+
+    }
+}
