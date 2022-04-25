b0ca99614595f070f3595c435f1dec15e6f0ccac
ruff <egoriktheruff@gmail.com>
2022-04-25 14:44:46 +0200

    Redesign and rework

---
+++ SEM/BinaryTree.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CompressionMethods
+{
+    class Leave : IComparable<Leave>
+    {
+        private uint frequency;
+        private byte character;
+        private ushort leftChild;
+        private ushort rightChild;
+        public Leave(uint _frequency, byte _character)
+        {
+            frequency = _frequency;
+            character = _character;
+            leftChild = 0;
+            rightChild = 0;
+        }
+        public Leave(uint _frequency, ushort _leftChild, ushort _rightChild)
+        {
+            frequency = _frequency;
+            character = (byte)'~';
+            leftChild = _leftChild;
+            rightChild = _rightChild;
+        }
+        public uint Frequency { get => frequency; }
+        public ushort LeftChild { get => leftChild; }
+        public ushort RightChild { get => rightChild; }
+        public byte Character { get => character; }
+
+        public bool isSymbol() => leftChild == rightChild ? true : false;
+        public int CompareTo([AllowNull] Leave other)
+        {
+            if (this.frequency < other.frequency)
+                return -1;
+            else if (this.frequency > other.frequency)
+                return 1;
+            else
+                return 0;
+        }
+        public override string ToString()
+        {
+            if (leftChild == 0 && rightChild == 0)
+                return frequency.ToString() + "-" + character + "_" + leftChild.ToString() + rightChild.ToString();
+            else
+                return frequency.ToString() + "-" + ":O:_" + leftChild.ToString() + rightChild.ToString(); ;
+            return base.ToString();
+        }
+    }
+    class Tree
+    {
+        private List<Leave> tree;
+        private ushort pointer;
+        private Dictionary<byte, string> alphabet;
+        public Tree(byte[] bytes)
+        {
+            pointer = 0;
+            uint[] probabilityTable = new uint[256];
+            foreach (byte b in bytes)
+                probabilityTable[b] += 1;
+            tree = new List<Leave>();
+            alphabet = new Dictionary<byte, string>();
+            for (int i = 0; i < probabilityTable.Length; i++)
+            {
+                if (probabilityTable[i] > 0)
+                    tree.Add(new Leave(probabilityTable[i], (byte)i));
+            }
+            tree.Sort();
+        }
+        public void makeTree()
+        {
+            while (pointer + 1 < tree.Count)
+            {
+                tree.Add(new Leave(tree[pointer].Frequency + tree[pointer + 1].Frequency, pointer, (ushort)(pointer + 1)));
+                tree.Sort();
+                pointer += 2;
+            }
+        }
+        private void treeBypass(ushort leaveId, string elementCode)
+        {
+            if (tree[leaveId].isSymbol())
+                alphabet.Add(tree[leaveId].Character, elementCode);
+            else
+            {
+                treeBypass(tree[leaveId].LeftChild, elementCode + "0");
+                treeBypass(tree[leaveId].RightChild, elementCode + "1");
+            }
+        }
+        public Dictionary<byte, string> getAlphabet()
+        {
+            treeBypass((ushort)(tree.Count - 1), "");
+            return alphabet;
+        }
+        public BitWriter serialize()
+        {
+            byte indexPower = getIndexPower(tree.Count - 1);
+            BitWriter br = new BitWriter(indexPower);
+
+            for (short i = 0; i < tree.Count; i++)
+            {
+                br.addBits(tree[i].Character);
+                br.addBits(tree[i].LeftChild, indexPower);
+                br.addBits(tree[i].RightChild, indexPower);
+            }
+            br.finalize();
+            return br;
+        }
+        private byte getIndexPower(int s)
+        {
+            for (byte i = 15; i >= 0; i--)
+            {
+                if ((s & (1 << i)) > 0)
+                    return (byte)(i + 1);
+            }
+            return 0;
+        }
+    }
+}
---
+++ SEM/BitOperations.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CompressionMethods
+{
+    class BitWriter
+    {
+        private int bitPtr;
+        private int bytePtr;
+        private List<byte> byteArray;
+
+        public int BitPtr { get => bitPtr; }
+        public List<byte> ByteArray { get => byteArray; }
+
+        public BitWriter() : this(0) { }
+        public BitWriter(byte preamble)
+        {
+            bitPtr = 0;
+            bytePtr = 1;
+            byteArray = new List<byte> { };
+            byteArray.Add((byte)(preamble << 3));
+            byteArray.Add(0);
+        }
+        private byte getBit(char c)
+        {
+            if (c == 48)
+                return 0;
+            else if (c == 49)
+                return 1;
+            else
+                throw new Exception("Wrong number");
+        }
+        public byte getByte(string s)
+        {
+            byte b = 0;
+            for (int i = s.Length - 1, j = 0; i >= 0; i--, j++)
+                b |= (byte)(getBit(s[i]) << j);
+            return b;
+        }
+        public void addBits(string s)
+        {
+            string sub = "";
+            //int bitCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                sub += s[i];
+                if (sub.Length == 8)
+                {
+                    addBits(getByte(sub), (byte)sub.Length);
+                    sub = "";
+                }
+            }
+            if (sub.Length > 0)
+                addBits(getByte(sub), (byte)sub.Length);
+        }
+        public void addBits(byte b) => addBits(b, 8);
+        public void addBits(byte b, byte size)
+        {
+            checkCompatible(size, sizeof(byte));
+            if (bitPtr + size > sizeof(byte) * 8)
+            {
+                byteArray.Add(0);
+                int nextByteBitPtr = size - (sizeof(byte) * 8 - bitPtr);
+                byteArray[bytePtr] = (byte)(byteArray[bytePtr] | (byte)(b >> nextByteBitPtr));
+                int mask = (2 << nextByteBitPtr) - 1;
+                byteArray[bytePtr + 1] = (byte)(
+                    (b & mask) << (sizeof(byte) * 8 - nextByteBitPtr)
+                    );
+                bitPtr = nextByteBitPtr;
+                bytePtr++;
+            }
+            else
+            {
+                byteArray[bytePtr] = (byte)(byteArray[bytePtr] | (byte)(b << sizeof(byte) * 8 - size - bitPtr));
+                bitPtr += size;
+            }
+        }
+        public void addBits(ushort s, byte size)
+        {
+            if (size <= 8)
+                addBits((byte)s, size);
+            else
+            {
+                checkCompatible(size, sizeof(ushort));
+                addBits((byte)(s >> 8), (byte)(size - 8));
+                addBits((byte)(s & 0xff), 8);
+            }
+        }
+        public void toFile(string address)
+        {
+            FileStream fs = File.OpenWrite(address);
+
+            byte[] outBytes = new byte[byteArray.Count];
+
+            fs.Write(genHeader(), 0, 12);
+            fs.Write(byteArray.ToArray(), 0, byteArray.Count);
+            fs.Close();
+        }
+        public void finalize() => byteArray[0] |= (byte)bitPtr;
+        public byte[] genHeader()
+        {
+            const short magicBytes = 12345;
+            long bitTreeLength = long.MaxValue;
+            long bitDataLength = bytePtr * 8 + bitPtr;
+            int headLength = sizeof(short) + 2 * sizeof(long);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    bw.Write(magicBytes);
+                    bw.Write(bitTreeLength);
+                    bw.Write(bitDataLength);
+                }
+                return ms.ToArray();
+            }
+
+        }
+        private void checkCompatible(byte size, int power)
+        {
+            if (size > power* 8)
+                throw new Exception("Power is lower than digits");
+        }
+        public override string ToString()
+        {
+            string result = "";
+            foreach (byte b in byteArray)
+            {
+                for (int i = 7; i >= 0; i--)
+                    result += 1 & (b >> i);
+                result += " | ";
+            }
+            return result;
+        }
+    }
+    class BitReader
+    {
+        private int bitPtr;
+        private int bytePtr;
+        private byte[] byteArray;
+
+        public BitReader(byte[] inputBytes, int offset)
+        {
+            byteArray = inputBytes;
+        }
+    }
+}
